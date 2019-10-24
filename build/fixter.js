e85e961f233bce373fc7fed078e034670236407d
Jorge Antonio HG <jorgeantoniohernandezguzman@gmail.com>
2019-10-24 14:15:45 -0500

    feat(*): Add reducer and store

--- build/fixter.js
+++ build/fixter.js
@@ -91,10 +91,91 @@
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var redux__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+//import * as Redux from 'redux';
 
 
-console.log(redux__WEBPACK_IMPORTED_MODULE_0__);
-console.log('init')
+//nodes
+let input = document.getElementById("thingsInput");
+let todos = {}
+let list = document.getElementById("list");
+
+//function
+function drawTodos() {
+    list.innerHTML = "";
+    // Update todos
+    todos = store.getState();
+    for( let key in todos) {
+        let li = document.createElement('li');
+        let classDone = todos[key].done ? "done" : "";
+        li.innerHTML = `
+            <span id="${key}" class="${classDone}"> ${todos[key].text} </span>
+            <span data-id="${key}" data-action="delete"> X </span>`;
+        setListeners(li);
+        list.appendChild(li);
+    }
+}
+
+function setListeners(li) {
+    li.addEventListener("click", e => {
+        if (e.target.getAttribute("data-action") === "delete") {
+            let key = e.target.getAttribute("data-id");
+            store.dispatch({
+                type: "DELETE_TODO",
+                id: key
+            });
+            return
+        }
+        let key = e.target.id;
+        todos[key].done = !todos[key].done;
+        store.dispatch({
+            type: "UPDATE_TODO",
+            todo: todos[key]
+        })
+    });
+}
+
+
+//listeners
+input.addEventListener("keydown", e => {
+    if (e.key === "Enter") {
+        let text = e.target.value;
+        let todo = {text, done:false};
+        store.dispatch({
+            type: "ADD_TODO",
+            todo
+        })
+    }
+})
+
+//REDUX
+
+// Reducer
+function todosReducer(state = { lol: true}, action) {
+    switch(action.type) {
+        case "ADD_TODO":
+            action.todo["id"] = Object.keys(state).length;
+            return {...state, [Object.keys(state).length]:action.todo};
+        case "UPDATE_TODO":
+            return {...state, [action.todo.id]:action.todo}
+        case "DELETE_TODO":
+            delete state[action.id]
+            return {...state}
+        default:
+            return state;
+    }
+}
+
+// store
+let store = Object(redux__WEBPACK_IMPORTED_MODULE_0__["createStore"])(todosReducer, {
+    0: {
+        text: "create store",
+        done: "true",
+        id: 0
+    }
+});
+
+store.subscribe(drawTodos);
+drawTodos();
 
 /***/ }),
 /* 1 */
--- main.js
+++ main.js
@@ -1,4 +1,85 @@
-import * as Redux from 'redux';
+//import * as Redux from 'redux';
+import { createStore } from 'redux';
 
-console.log(Redux);
-console.log('init')
+//nodes
+let input = document.getElementById("thingsInput");
+let todos = {}
+let list = document.getElementById("list");
+
+//function
+function drawTodos() {
+    list.innerHTML = "";
+    // Update todos
+    todos = store.getState();
+    for( let key in todos) {
+        let li = document.createElement('li');
+        let classDone = todos[key].done ? "done" : "";
+        li.innerHTML = `
+            <span id="${key}" class="${classDone}"> ${todos[key].text} </span>
+            <span data-id="${key}" data-action="delete"> X </span>`;
+        setListeners(li);
+        list.appendChild(li);
+    }
+}
+
+function setListeners(li) {
+    li.addEventListener("click", e => {
+        if (e.target.getAttribute("data-action") === "delete") {
+            let key = e.target.getAttribute("data-id");
+            store.dispatch({
+                type: "DELETE_TODO",
+                id: key
+            });
+            return
+        }
+        let key = e.target.id;
+        todos[key].done = !todos[key].done;
+        store.dispatch({
+            type: "UPDATE_TODO",
+            todo: todos[key]
+        })
+    });
+}
+
+
+//listeners
+input.addEventListener("keydown", e => {
+    if (e.key === "Enter") {
+        let text = e.target.value;
+        let todo = {text, done:false};
+        store.dispatch({
+            type: "ADD_TODO",
+            todo
+        })
+    }
+})
+
+//REDUX
+
+// Reducer
+function todosReducer(state = { lol: true}, action) {
+    switch(action.type) {
+        case "ADD_TODO":
+            action.todo["id"] = Object.keys(state).length;
+            return {...state, [Object.keys(state).length]:action.todo};
+        case "UPDATE_TODO":
+            return {...state, [action.todo.id]:action.todo}
+        case "DELETE_TODO":
+            delete state[action.id]
+            return {...state}
+        default:
+            return state;
+    }
+}
+
+// store
+let store = createStore(todosReducer, {
+    0: {
+        text: "create store",
+        done: "true",
+        id: 0
+    }
+});
+
+store.subscribe(drawTodos);
+drawTodos();
